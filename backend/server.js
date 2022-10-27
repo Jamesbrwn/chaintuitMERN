7da0cfc027bde495400f6d92702570c827cdb1a4
jimmyslim22 <brown.jmsg@gmail.com>
2022-10-27 14:23:16 -0700

    Updated vercel file

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,25 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
+// Stytch integration
+const client = new stytch.Client({
+    project_id: "project-test-a92932a9-538f-429a-9ea3-83e519819dfa" ,
+    secret: "secret-test-Tsv5EeJekpt0fywttSKdEsBpTqAslqx8toY=",
+    env: stytch.envs.test,
+})
+
+app.post("/login", async (req, res) =>{
+    const email = req.body.email
+    const params = {
+        email,
+        login_magic_link_url: "http://localhost:3000/auth",
+        signup_magic_link_url: "http://localhost:3000/auth"
+    }
+
+    const response = await client.magicLinks.email.loginOrCreate()
+    res.json(response);
+})
+
 app.use('/api/users', require('./routes/userRoutes'))
-
 app.use(errorHandler)
-
 app.listen(port, () => console.log(`Server started on ${port}`))
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react';
+import Axios from 'axios';
 
 function Login() {
+    const [email, setEmail] = useState("");
+
+    const login = () => {
+        Axios.post("http://localhost:5000/login", {email}).then(() => {
+            console.log("ok");
+         });
+    };
   return (
     <div className="formContainer">
          <div>
@@ -8,8 +16,14 @@ function Login() {
             </div>
             <div className="body">
                 <p> Enter your email to sign up.</p>
-                <input type="email" placeholder="Email..."/>
-                <button> Login </button>
+                <input 
+                    type="email" 
+                    placeholder="Email..." 
+                    onChange={(e) => {
+                        setEmail(e.target.value)
+                    }}
+                    />
+                <button onClick={login}> Login </button>
             </div>
         </div>
   )
